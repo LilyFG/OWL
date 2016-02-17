ca952e62f4683797d179460c7715ad624a97d45f
Lily FitzGibbon <Lily@Lily-FitzGibbons-MacBook-Pro.local>
2016-02-17 17:59:50 +0000

    update syntax for dependent packages

--- R/import.owl.R
+++ R/import.owl.R
@@ -1,17 +1,17 @@
 #' import OWL data, flatten and return a list of data split by subject
-#' 
+#'
 #' @param choose is a logical where TRUE means you have to select the file and FALSE means it searches for the "results.json" file, defaults to FALSE
-#' 
+#'
 #' @return a list contatining all the data for each participant
-#' 
+#'
 #' @examples
 #' import.owl(choose = FALSE)
 #' @export
 import.owl <- function(choose = F){
   file <- ifelse(choose, file.choose(), "results.data")
   #import the data file
-  dat <- fromJSON(file, flatten = T)
-  
+  dat <- jsonlite::fromJSON(file, flatten = T)
+
   #split the file by subject
   split(dat, as.factor(dat$`_id`))
 }
