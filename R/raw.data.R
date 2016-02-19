72e66811822532c9cac61cc7d046e67230e89bbf
Lily FitzGibbon <Lily@Lily-FitzGibbons-MacBook-Pro.local>
2016-02-19 14:48:47 +0000

    "update manuals" :w :q

--- R/raw.data.R
+++ R/raw.data.R
@@ -1,14 +1,14 @@
 #' create a data frame containing all the raw data for the participants
-#' 
+#'
 #' @param task is the task of interest
 #' @param data is the list of subject data (defaults to bySubject)
-#' 
+#'
 #' @return a data frame containing raw data for all participants who have completed the task
-#' 
+#'
 #' @examples
 #' raw.data(bySubject, "fp")
 #' @export
-#' 
+#'
 raw.data <- function(data, task, csv = F){
   raw.list <- lapply(data, split.task.results, task = task)
   add.names <- function(subject, data){
@@ -19,5 +19,10 @@ raw.data <- function(data, task, csv = F){
                      subject = names(raw.list),
                      data = raw.list)
   raw.df <- data.table::rbindlist(l = raw.list, fill = T)
-  if(csv) write.csv(paste(task, Sys.Date(), ".csv"), raw.data) else   raw.df
+  if(csv){
+    lapply(raw.data, function(x){
+      if(is.list(x)) as.character(x)
+    })
+    write.csv(paste(task, Sys.Date(), ".csv"), raw.data)
+    } else   raw.df
 }
