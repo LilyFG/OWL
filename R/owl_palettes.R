c2a993a46a1c3e173aafda1a6f2f02337edbf63d
LilyFG <l.fitzgibbon@bham.ac.uk>
2016-02-19 10:13:52 +0000

    Added files via upload

---
+++ R/owl_palettes.R
@@ -0,0 +1,10 @@
+#' Complete list of palettes
+#'
+#' @export
+owl_palettes <- list(
+  owl_pal1 = c('#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45','#006d2c','#00441b'),
+  owl_pal2 = c('#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#045a8d','#023858'),
+  owl_pal3 = c('#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd'),
+  owl_pal4 = c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'),
+  owl_pal5 = c('#f7fcfd','#e0ecf4','#bfd3e6','#9ebcda','#8c96c6','#8c6bb1','#88419d','#810f7c','#4d004b')
+)
