16bb1d0235244c056b600d679ec2a553c8657b93
Lily FitzGibbon <Lily@Lily-FitzGibbons-MacBook-Pro.local>
2016-02-22 15:30:48 +0000

    add demo

--- R/raw.data.R
+++ R/raw.data.R
@@ -13,14 +13,6 @@ raw.data <- function(data, task){
   raw.list <- lapply(data, split.task.results, task = task)
   raw.list <- lapply(raw.list, add.variables, task)
 
-  #   add.demo <- function(data){
-  #     if(!is.null(data)) {
-  #     }
-  #   }
-  #   if(task!= "demo"){
-  #     raw.list <- lapply(raw.list, add.demo)
-  #   }
-
   add.names <- function(subject, subject.number, data){
     if(!is.null(data)) {
       data.frame(subject.id = rep(subject, nrow(data)), subject.number = rep(subject.number, nrow(data)), data)
@@ -28,10 +20,23 @@ raw.data <- function(data, task){
 
   }
 
+  demo <- demo.extract(bySubject)
+
+  raw.list <- mapply(FUN = function(raw.data, demo.data){
+    if(!is.null(raw.data)){
+      data.frame(raw.data, rep(demo, nrow(raw.data)))
+    }
+  },
+  raw.list,
+  demo)
+
   # apply the add.names function to the raw data taking data, list names and list sequence as arguments
   raw.list <- mapply(FUN = add.names,
                      subject = names(raw.list),
                      subject.number = seq(1, length(raw.list), by = 1),
                      data = raw.list)
+
+
+
   data.frame(data.table::rbindlist(l = raw.list, fill = T))
 }
