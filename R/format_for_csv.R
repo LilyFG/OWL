46ac6f3d6ad53e4d52aa79b2a18748ee597276f7
Lily FitzGibbon <Lily@Lily-FitzGibbons-MacBook-Pro.local>
2016-02-22 11:37:26 +0000

    documentation for new functions

--- R/format_for_csv.R
+++ R/format_for_csv.R
@@ -1,4 +1,11 @@
-
+#' format_for_csv flattens any lists and reformats strings so Excel doesn't read them as dates
+#'
+#' @param raw.data is a data frame of raw data for one task
+#'
+#' @return a data frame ready to be output as a csv file
+#'
+#' @export
+#'
 # this is done on the assumption that we will deal with these list variables at a later stage
 format_for_csv <- function(raw.data){
   flat <- lapply(raw.data, function(x){
