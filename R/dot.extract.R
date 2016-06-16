dfefa404e2a50b44cf316ead5f3d0f0312e15502
LilyFG <l.fitzgibbon@bham.ac.uk>
2016-06-16 09:08:11 +0100

    Update dot.extract.R
    Moved trimming to the top
    Added N responses less than 300ms
    Added binomial test result

--- R/dot.extract.R
+++ R/dot.extract.R
@@ -6,31 +6,35 @@ dot.extract <- function(df){
   lapply(dot, function(dot){
   
     if(!is.null(dot)){
+    
+      # trim out practice, filler and 'no' trials
+      dot <- dot[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes', ]
+      
+      
       # recode response data as 1=correct, 0=incorrect and NA=timed out
       dot$accuracy[dot$response == dot$trial.correctResponse] <- 1
       dot$accuracy[is.na(dot$accuracy) & dot$response %in% c("yes","no")] <- 0
       
-      # create a new dataframe for the accuracy data including only 'neutral' (not 'filler') trials and where the correct response was 'yes'
-      dot.acc <- reshape::melt.data.frame(dot[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes', 
-      # include the accuracy variable and the two trial type variables: self/other and consistency
-                          c("accuracy", "trial.selfOther", "trial.consistency")], 
+      # calculate the binomial test to determine whether N correct on 'self' trials is greater than chance
+      dot.test <- binom.test(sum(dot$accuracy[dot$trial.selfOther=="self"] == 1, na.rm=T), sum(dot$accuracy[dot$trial.selfOther=="self"] %in% c(0,1), na.rm=T), p = 0.5, alternative = "greater")
+      
+      # create a new dataframe for the accuracy data including only the accuracy variable and the two trial type variables: self/other and consistency
+      dot.acc <- reshape::melt.data.frame(dot[, c("accuracy", "trial.selfOther", "trial.consistency")], 
       # use the self/other and consistency variables to reshape the data from long into wide format
                       id=c("trial.selfOther", "trial.consistency"),
                       na.rm=TRUE)
       # calculate the mean accuracy including only trials for which a response was made
       dot.acc <- as.data.frame(reshape::cast(dot.acc, trial.selfOther~trial.consistency~variable, mean))
       
-      # create a new dataframe for the RT data including only 'neutral' (not 'filler') trials and where the correct response was 'yes' and the response was accurate
-      dot.RT <- reshape::melt.data.frame(dot[dot$phase==3 & dot$accuracy==1 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes',  
-                         c("responseTime", "trial.selfOther", "trial.consistency")], 
+      # create a new dataframe for the RT data including only trials where the response was accurate
+      dot.RT <- reshape::melt.data.frame(dot[dot$accuracy==1, c("responseTime", "trial.selfOther", "trial.consistency")], 
                      id=c("trial.selfOther", "trial.consistency"),
                      na.rm=TRUE)
-                     
-      
+      # calculate the mean RT including only trials for which a response was made
       dot.RT <- as.data.frame(reshape::cast(dot.RT, trial.selfOther~trial.consistency~variable, mean))
       
-      data.frame(dot.self.total.acc=length(dot$accuracy[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & "trial.selfOther"=="self"]), 
-                 dot.total.timeouts=length(dot$response[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & dot$response=="noresponse"]),
+      data.frame(dot.self.total.acc=length(dot$accuracy["trial.selfOther"=="self"]), 
+                 dot.total.timeouts=length(dot$response[dot$response=="noresponse"]),
                  dot.other.con.acc=dot.acc["other", "con.accuracy"],
                  dot.other.incon.acc=dot.acc["other", "incon.accuracy"], 
                  dot.self.con.acc=dot.acc["self", "con.accuracy"],
@@ -41,10 +45,15 @@ dot.extract <- function(df){
                  dot.self.incon.RT=dot.RT["self", "incon.responseTime"],  
                  
                  # calculate N timeouts for each trial type on critical trials
-                 dot.other.con.timeouts=length(dot$response[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & dot$response=="noresponse" & "trial.selfOther"=="other" & "trial.consistency"=="con"]),
-                 dot.other.incon.timeouts=length(dot$response[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & dot$response=="noresponse" & "trial.selfOther"=="other" & "trial.consistency"=="incon"]),
-                 dot.self.con.timeouts=length(dot$response[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & dot$response=="noresponse" & "trial.selfOther"=="self" & "trial.consistency"=="con"]),
-                 dot.self.incon.timeouts=length(dot$response[dot$phase==3 & dot$trial.trialType=='neutral' & dot$trial.correctResponse=='yes' & dot$response=="noresponse" & "trial.selfOther"=="self" & "trial.consistency"=="incon"])
+                 dot.other.con.timeouts=length(dot$response[dot$response=="noresponse" & "trial.selfOther"=="other" & "trial.consistency"=="con"]),
+                 dot.other.incon.timeouts=length(dot$response[dot$response=="noresponse" & "trial.selfOther"=="other" & "trial.consistency"=="incon"]),
+                 dot.self.con.timeouts=length(dot$response[dot$response=="noresponse" & "trial.selfOther"=="self" & "trial.consistency"=="con"]),
+                 dot.self.incon.timeouts=length(dot$response[dot$response=="noresponse" & "trial.selfOther"=="self" & "trial.consistency"=="incon"]),
+                 
+                 dot.Ntrials.sub300ms=sum(dot$responseTime<300),
+                 
+                 # report whether the p-value of the binomial test was below 0.05, i.e. performance on 'self' trials was above chance (TRUE)
+                 dot.above.chance=dot.test$p.value<0.05
       )
     }
   })
